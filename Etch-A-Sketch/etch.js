73a53427caeb317b8fd99346899ed6d27aaf756c
Maia Petee <maiapetee@OS-X-Mavericks.local>
2015-04-10 21:28:11 -0600

    It Works

--- Etch-A-Sketch/etch.js
+++ Etch-A-Sketch/etch.js
@@ -32,8 +32,25 @@ $(document).ready(function() {
 
     var reset = function() {
         $("button").click(function() {
-            var ask = prompt("How many squares would you like alongside your grid? Enter a number 2 to 100.");
+            var $value = $("input").val();
+
+            if($value > 1 && $value < 51) {
+                var $conSide = $(".container").width();
+                var $pixelSide = ($conSide / $value);
+                var $pixelNum = ($conSide / $pixelSide) * ($conSide / $pixelSide);
+
+                for(var $i = 1; $i < ($pixelNum + 1); $i++) {
+                    $(".container").append("<div class='pixel'></div>");
+                }
+
+                $(".pixel").width($pixelSide-2).height($pixelSide-2);
+
+                hover();
+            } else {
+                alert("That is not a number between 2 and 50! Please try again.");
+            }
         });
+
 
     }
 
